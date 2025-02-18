0bae8237d3c7d80bdec6800ac1e89a63faccdcf8
Anton Egorov <mous8890@gmail.com>
2025-02-19 00:58:49 +0400

    client-server app

---
+++ chat-client/src/main/java/web/app/client/Main.java
@@ -0,0 +1,12 @@
+package web.app.client;
+
+import web.app.client.view.ClientUI;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ClientUI view = new ClientUI();
+    }
+}
---
+++ chat-client/src/main/java/web/app/client/model/Client.java
@@ -0,0 +1,90 @@
+package web.app.client.model;
+
+import web.app.client.view.ClientUI;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private Socket socket;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    private String name;
+    private ClientUI view;
+
+    public Client(Socket socket, String name, ClientUI view) {
+        this.socket = socket;
+        this.name = name;
+        try {
+            //переход от массива байт в строковый буфер
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            //UI
+            this.view = view;
+            //если соединение потеряно
+        } catch (IOException e) {
+            closeEverything(socket, reader, writer);
+        }
+    }
+
+    /**
+     * Метод отправки сообщений от клиента
+     */
+    public void sendMessage(String message) {
+        try {
+            if (!socket.isClosed()) {
+                writer.write(message);
+                writer.newLine();
+                writer.flush();
+            }
+        } catch (IOException e) {
+            closeEverything(socket, reader, writer);
+        }
+    }
+
+    /**
+     * Метод ожидания сообщений в чате (в отдельном потоке)
+     */
+    public void listenForMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String messageFromGroup;
+                while (!socket.isClosed()) {
+                    try {
+                        //здесь работа будет приостановлена пока в буфере что-то не появится
+                        messageFromGroup = reader.readLine();
+                        view.handleMessage(messageFromGroup);
+                    } catch (IOException e) {
+                        closeEverything(socket, reader, writer);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * Метод закрытия соединения и всех потоков ввода и вывода
+     */
+    private void closeEverything(Socket socket, BufferedReader reader, BufferedWriter writer) {
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+            if (reader != null) {
+                reader.close();
+            }
+            if (writer != null) {
+                writer.close();
+            }
+            view.handleMessage("Вы были отключены от сервера.");
+            view.getConnectionComponent().setVisible(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
